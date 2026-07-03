8f7111f805c31b07461beaccb63d8018716f30a5
rohansamanta23 <rohansamanta24@gmail.coAm>
2026-07-03 12:13:17 +0530

    1st initialization

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>PROJECT-C01</h1>
+      <h2>Merchant Inventory & Order Dashboard-</h2>
+      <p>Standard e-commerce sites are overdone. Instead, build the software that the store owner uses.</p>
+      <p><b>The Concept:</b> A secure admin portal where a merchant can add/edit products, manage stock levels, and view incoming orders.</p>
+      <p><b>Why it looks good on a CV:</b> Enterprise companies love developers who understand business logic. You can implement features like low-stock alerts, role-based access control (Admin vs. Employee permissions), and even generate PDF invoices.</p>
+    </>
+  )
+}
+
+export default App
